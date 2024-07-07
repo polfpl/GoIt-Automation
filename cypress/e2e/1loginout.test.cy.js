22bae5fa513141909ffad7d00a6ff0c64eefae5c
Paweł Firlej <polf@polf.pl>
2024-07-07 14:08:52 +0200

    loginout
    polishing

--- cypress/e2e/1loginout.test.cy.js
+++ cypress/e2e/1loginout.test.cy.js
@@ -5,19 +5,12 @@ describe('User can visit GoIt page', () => {
 });
 
 it('Log in and log out ', () => {
-    // find user email and type mail
-    cy.get('#user_email').type('user888@gmail.com')
-    // find user password and type password
-    cy.get('#user_password').type('1234567890')
-    // find button and click
-    cy.get('.next-1jphuq5').click()
-    // find navigation menu button
-    cy.get('#open-navigation-menu-mobile')
-    // click navigation menu button
-    cy.get('#open-navigation-menu-mobile').click()
-    // find logout button 
-    cy.get(':nth-child(12) > .next-bve2vl')
-    // click logout button
-    cy.get(':nth-child(12) > .next-bve2vl').click()
-});
+    cy.get('#user_email').type('user888@gmail.com')// find user email and type mail
+    cy.get('#user_password').type('1234567890')// find user password and type password
+    cy.get('.next-1jphuq5').click()// find button and click
+    cy.get('#open-navigation-menu-mobile') // find navigation menu button
+    cy.get('#open-navigation-menu-mobile').click()// click navigation menu button
+    cy.get(':nth-child(12) > .next-bve2vl')// find logout button 
+    cy.get(':nth-child(12) > .next-bve2vl').click()// click logout button
+    });
 });
--- cypress/e2e/2loginout.test.cy.js
+++ cypress/e2e/2loginout.test.cy.js
@@ -1,23 +1,16 @@
-describe('Wejście na stronę GoIt', () => {
-    // visit page
-    beforeEach('Go to page', () => {
-    cy.visit('https://www.edu.goit.global/pl/account/login');
-});
+describe("Wejście na stronę GoIt", () => {
+  // wejście na stronę
+  beforeEach("Go to page", () => {
+    cy.visit("https://www.edu.goit.global/pl/account/login");
+  });
 
-it('Zalogowanie i wylogowanie ', () => {
-    // znalezienie pola email i wpisanie email użytkownika
-    cy.get('#user_email').type('testowyqa@qa.team')
-    // znalezienie pola password i wpisanie hasła
-    cy.get('#user_password').type('QA!automation-1')
-    // znalezienie przycisku Login i kliknięcie - zalogowanie
-    cy.get('.next-1jphuq5').click()
-    // znalezienie przycisku navigation menu
-    cy.get('#open-navigation-menu-mobile')
-    // liknięcie przycisku navigation menu
-    cy.get('#open-navigation-menu-mobile').click()
-    // znalezienie przycisku logout 
-    cy.get(':nth-child(9) > .next-bve2vl')
-    // kliknięsie przycisku wylogowania - wylogowanie 
-    cy.get(':nth-child(9) > .next-bve2vl').click()
-   });
+  it("Zalogowanie i wylogowanie ", () => {
+    cy.get("#user_email").type("testowyqa@qa.team"); // znalezienie pola email i wpisanie email użytkownika
+    cy.get("#user_password").type("QA!automation-1"); // znalezienie pola password i wpisanie hasła
+    cy.get(".next-1jphuq5").click(); // znalezienie przycisku Login i kliknięcie - zalogowanie
+    cy.get("#open-navigation-menu-mobile"); // znalezienie przycisku navigation menu
+    cy.get("#open-navigation-menu-mobile").click(); // kliknięcie przycisku navigation menu
+    cy.get(":nth-child(9) > .next-bve2vl"); // znalezienie przycisku logout
+    cy.get(":nth-child(9) > .next-bve2vl").click(); // kliknięsie przycisku wylogowania - wylogowanie
+  });
 });

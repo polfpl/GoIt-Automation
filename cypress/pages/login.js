a9fb83e91bcc1f9858b00c7f6e8fd8985cf0412f
Paweł Firlej <polf@polf.pl>
2024-07-17 00:15:24 +0200

    Update login.js

--- cypress/pages/login.js
+++ cypress/pages/login.js
@@ -1,18 +1,21 @@
 export class Login {
-  visit() {
-    cy.visit("https://www.edu.goit.global/account/login");
-  }
+    visit() {
+      cy.visit("https://www.edu.goit.global/account/login");
+    }
+    
+    login() {
+        cy.get("#user_email").type("testowyqa@qa.team"); // znalezienie pola email i wpisanie email użytkownika
+        cy.get("#user_password").type("QA!automation-1"); // znalezienie pola password i wpisanie hasła
+        cy.get(".next-1jphuq5").click(); // znalezienie przycisku Login i kliknięcie - zalogowanie
+        
+    }
+    
+    logout() {
+        cy.get("#open-navigation-menu-mobile"); // znalezienie przycisku navigation menu
+        cy.get("#open-navigation-menu-mobile").click(); // kliknięcie przycisku navigation menu
+        cy.get(":nth-child(9) > .next-bve2vl"); // znalezienie przycisku logout
+        cy.get(":nth-child(9) > .next-bve2vl").click(); // kliknięsie przycisku wylogowania - wylogowanie
+    }  
 
-  login() {
-    cy.get("#user_email").type("testowyqa@qa.team"); // znalezienie pola email i wpisanie email użytkownika
-    cy.get("#user_password").type("QA!automation-1"); // znalezienie pola password i wpisanie hasła
-    cy.get(".next-1jphuq5").click(); // znalezienie przycisku Login i kliknięcie - zalogowanie
-  }
-
-  logout() {
-    cy.get("#open-navigation-menu-mobile"); // znalezienie przycisku navigation menu
-    cy.get("#open-navigation-menu-mobile").click(); // kliknięcie przycisku navigation menu
-    cy.get(":nth-child(9) > .next-bve2vl"); // znalezienie przycisku logout
-    cy.get(":nth-child(9) > .next-bve2vl").click(); // kliknięsie przycisku wylogowania - wylogowanie
-  }
 }
+

dcabc45570f42cfaa8d55e0d37d3f6d495c3b7af
Paweł Firlej <polf@polf.pl>
2024-07-16 23:58:35 +0200

    Update HomePage.cy.js

--- cypress/e2e/HomePage.cy.js
+++ cypress/e2e/HomePage.cy.js
@@ -3,15 +3,14 @@ import { Login } from "../pages/login";
 const loginPage = new Login();
 
 describe("Page login object example", () => {
-        
-    beforeEach("Go to page", () => {
-        loginPage.visit();
-      });
-    
-    it("login page test", () => {
-        // login
-        loginPage.login();
-        // logout
-        loginPage.logout();
+  beforeEach("Go to page", () => {
+    loginPage.visit();
+  });
+
+  it("login page test", () => {
+    // login
+    loginPage.login();
+    // logout
+    loginPage.logout();
   });
 });
